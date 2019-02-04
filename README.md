8eea5dfe412fedf921591f27d4ac958fcd3d67fc
Noe Casas <noe.casas@gmail.com>
2019-02-04 13:40:50 +0100

    Add readme and python code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # fairseq-tensorboard
-Small utility to monitor fairseq training in tensorboard
+
+This is a small utility to monitor fairseq training in tensorboard.
+
+It is not a fork of fairseq, but just a small class that extends its
+functionality with tensorboard logging.
+
+# Installation and Usage
+
+You just need to clone fairseq-tensorboard, install its only dependency
+([tensorboardX](https://github.com/lanpa/tensorboardX)) and launch
+fairseq's `train.py` specifying task `monitored_translation`:
+
+```
+pip install tensorboardX
+
+git clone https://github.com/noe/fairseq-tensorboard.git
+
+python fairseq/train.py \
+   --user-dir ./fairseq-tensorboard/fstb \
+   --task monitored_translation [...]
+```
---
+++ fstb/task.py
@@ -0,0 +1,32 @@
+import os
+
+from fairseq.tasks import register_task
+from fairseq.tasks.translation import TranslationTask
+
+from tensorboardX import SummaryWriter
+
+
+@register_task('monitored_translation')
+class MonitoredTranslationTask(TranslationTask):
+    def __init__(self, args, src_dict, tgt_dict):
+        super().__init__(args, src_dict, tgt_dict)
+        self.num_updates = 0
+        should_log = getattr(args, 'distributed_rank', 0) == 0
+        log_dir = os.path.join(args.save_dir, 'logs')
+        self.logger = SummaryWriter(log_dir) if should_log else None
+
+    def aggregate_logging_outputs(self, logging_outputs, criterion):
+        aggregated = super().aggregate_logging_outputs(logging_outputs, criterion)
+
+        if self.logger is not None:
+            is_training = criterion.training
+            prefix = 'train_' if is_training else 'valid_'
+            for key, value in aggregated.items():
+                tag = prefix + key
+                self.logger.add_scalar(tag, value, self.num_updates)
+
+        return aggregated
+
+    def train_step(self, sample, model, criterion, optimizer, ignore_grad=False):
+        self.num_updates += 1
+        return super().train_step(sample, model, criterion, optimizer, ignore_grad=ignore_grad)
